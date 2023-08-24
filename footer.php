671a706079fa8cac8634f8c3472483be8fd91257
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-24 22:57:15 +0600

    footer done

--- footer.php
+++ footer.php
@@ -1,9 +1,132 @@
 <footer class="footer">
-    <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm d-sm-inline-block">copyright ©
-            2019-<?php echo date("Y");?> sabbircommerce. All
-            rights reserved.</span>
+    <!-- Remove the container if you want to extend the Footer to full width. -->
+    <div class="">
+        <!-- Footer -->
+        <footer class="text-center text-lg-start text-dark" style="background-color: #ECEFF1">
+            <!-- Section: Social media -->
+            <section class="d-flex justify-content-between p-4 text-white" style="background-color: #5768f5">
+                <!-- Left -->
+                <div class="me-5">
+                    <h3>Social Networks </h3>
+                </div>
+                <!-- Left -->
+
+                <!-- Right -->
+                <div>
+                    <a href="" class="text-white me-4">
+                        <i class="mdi mdi-facebook"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="mdi mdi-twitter"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="mdi mdi-google"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="mdi mdi-instagram"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="mdi mdi-linkedin"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="mdi mdi-github"></i>
+                    </a>
+                </div>
+                <!-- Right -->
+            </section>
+            <!-- Section: Social media -->
+
+            <!-- Section: Links  -->
+            <section class="">
+                <div class="container text-center text-md-start mt-5">
+                    <!-- Grid row -->
+                    <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold">Company name</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                Here you can use rows and columns to organize your footer
+                                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Products</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-dark">MDBootstrap</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-dark">MDWordPress</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-dark">BrandFlow</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-dark">Bootstrap Angular</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Useful links</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-dark">Your Account</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-dark">Become an Affiliate</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-dark">Shipping Rates</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-dark">Help</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Contact</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                                style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p><i class="mdi mdi-home mr-3"></i> New York, NY 10012, US</p>
+                            <p><i class="mdi mdi-email-outline mr-3"></i> info@example.com</p>
+                            <p><i class="mdi mdi-phone mr-3"></i> + 01 234 567 88</p>
+                            <p><i class="mdi mdi-printer mr-3"></i> + 01 234 567 89</p>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+                <span class="text-muted d-block text-center text-sm d-sm-inline-block">copyright ©
+                    2019-<?php echo date("Y");?> sabbircommerce. All
+                    rights reserved.
+                </span>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
     </div>
+    <!-- End of .container -->
 </footer>
 <!-- partial -->
 </div>
