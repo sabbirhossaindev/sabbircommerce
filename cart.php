a5463667826e54f8835bd03c5cf8b3fab7a10a02
sabbir hossain <mdsabbir477470@gmail.com>
2023-10-05 19:58:20 +0600

    card added

--- cart.php
+++ cart.php
@@ -14,27 +14,28 @@
             </tr>
         </thead>
 
+
         <tbody>
             <?php
-      if (isset($_SESSION['cart'])) :
-        $i = 1;
-        foreach ($_SESSION['cart'] as $cart) :
-      ?>
+                if (isset($_SESSION['cart'])) :
+                $i = 1;
+                foreach ($_SESSION['cart'] as $cart) :
+            ?>
             <tr class="text-center">
                 <td><?php echo $i; ?> # </td>
                 <td> Product <?= $cart['pro_id']; ?></td>
                 <td>
-                    <form action="update.php" method="post">
+                    <form action="updatecard.php" method="post">
                         <input type="number" value="<?= $cart['qty']; ?>" name="qty" min="1">
                         <input type="hidden" name="upid" value="<?= $cart['pro_id']; ?>">
                 </td>
                 <td>
                     <input type="submit" name="update" value="Update" class="btn btn-sm btn-warning">
-                    </form>
                 </td>
                 <td><a class="btn btn-sm btn-danger" href="removecartitem.php?id=<?= $cart['pro_id']; ?>"
                         onclick=" return confirm('Are you sure you want to delete.');">Remove</a>
                 </td>
+                </form>
             </tr>
             <?php
           $i++;
